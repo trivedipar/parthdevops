b007d3b0dee36750fb2cca3d3154cbd8f57427d9
Parth Girish Trivedi <113145407+trivedipar@users.noreply.github.com>
2024-06-23 22:05:17 -0400

    Adding main.tf file

---
+++ main.tf
@@ -0,0 +1,125 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  version = "3.0.0"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_dns_hostnames = true
+}
+
+resource "aws_lb" "frontend" {
+  name               = "frontend-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [module.vpc.default_security_group_id]
+  subnets            = module.vpc.public_subnets
+
+  enable_deletion_protection = false
+}
+
+resource "aws_lb" "backend" {
+  name               = "backend-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [module.vpc.default_security_group_id]
+  subnets            = module.vpc.public_subnets
+
+  enable_deletion_protection = false
+}
+
+resource "aws_lb_target_group" "frontend" {
+  name     = "frontend-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
+}
+
+resource "aws_lb_target_group" "backend" {
+  name     = "backend-tg"
+  port     = 5000
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
+}
+
+resource "aws_lb_listener" "frontend" {
+  load_balancer_arn = aws_lb.frontend.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.frontend.arn
+  }
+}
+
+resource "aws_lb_listener" "backend" {
+  load_balancer_arn = aws_lb.backend.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.backend.arn
+  }
+}
+
+data "template_file" "container_definitions" {
+  template = file("${path.module}/container-definitions.json.tpl")
+
+  vars = {
+    react_app_api_service_url  = "http://${aws_lb.frontend.dns_name}"
+    database_url               = "mongodb://${aws_lb.backend.dns_name}:27017/"
+  }
+}
+
+resource "aws_ecs_task_definition" "task" {
+  family                   = "my-ecs-task"
+  container_definitions    = data.template_file.container_definitions.rendered
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+}
+
+resource "aws_ecs_service" "service" {
+  name            = "my-ecs-service"
+  cluster         = aws_ecs_cluster.cluster.id
+  task_definition = aws_ecs_task_definition.task.arn
+  desired_count   = 2
+
+  network_configuration {
+    subnets         = module.vpc.private_subnets
+    security_groups = [module.vpc.default_security_group_id]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.frontend.arn
+    container_name   = "frontend"
+    container_port   = 80
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.backend.arn
+    container_name   = "backend"
+    container_port   = 5000
+  }
+}
+
+output "frontend_url" {
+  value = aws_lb.frontend.dns_name
+}
+
+output "backend_url" {
+  value = aws_lb.backend.dns_name
+}
