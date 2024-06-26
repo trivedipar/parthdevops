2152cf295602e8e9e5a12393632dc3558b194151
Parth Girish Trivedi <113145407+trivedipar@users.noreply.github.com>
2024-06-26 03:34:02 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -269,7 +269,7 @@ resource "aws_iam_role" "ecs_task_execution_role" {
   }
 }
 
-resource "aws_iam_role_policy" "ecs_task_execution_policy" {
+resource "aws_iam_policy" "ecs_task_execution_policy" {
   name = "ecsTaskExecutionPolicy-unique-2"
   policy = jsonencode({
     Version = "2012-10-17",
@@ -323,12 +323,11 @@ resource "aws_iam_role_policy" "ecs_task_execution_policy" {
       }
     ]
   })
-  role = aws_iam_role.ecs_task_execution_role.name
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = aws_iam_role_policy.ecs_task_execution_policy.arn
+  policy_arn = aws_iam_policy.ecs_task_execution_policy.arn
 }
 
 # CloudWatch Log Groups
